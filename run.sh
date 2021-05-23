ffd8a9e540225f2675f47b9cfbd871192dd93ec4
tomerun <tomerunmail@gmail.com>
2021-05-23 21:10:07 +0900

    remove meaningless command

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@
 SEED_START=$((100 + "$AWS_BATCH_JOB_ARRAY_INDEX" * "$RANGE"))
 SEED_END=$((100 + "$AWS_BATCH_JOB_ARRAY_INDEX" * "$RANGE" + "$RANGE"))
 
-mkdir out
 crystal build --release -D local main.cr
 for (( i = $SEED_START; i < $SEED_END; i++ )); do
 	seed=$(printf "%04d" $i)
